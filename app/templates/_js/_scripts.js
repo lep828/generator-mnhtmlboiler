0e37ea1277b07680b5ba872d5f73be4608bf66e2
Michael Nguyen <aqmnguyen@gmail.com>
2016-05-29 23:56:49 +0100

    adding help to the getQS function

--- app/templates/_js/_scripts.js
+++ app/templates/_js/_scripts.js
@@ -7,7 +7,7 @@ $(document).ready(function(){
 });
 
 function init(){
-  getQS();
+  getQS(URL_PARAMS);
 }
 
 function removeScripts(raw){
@@ -21,15 +21,18 @@ function removeScripts(raw){
   return raw;
 }
 
-
-function getQS(){
-  var match;
+function getQS(object){
+  /*
+  Query string values get returned back to the object
+  Use object.key|object[key] //returns value
+  */
   var pl     = /\+/g;  // Regex for replacing addition symbol with a space
   var search = /([^&=]+)=?([^&]*)/g;
   var decode = function (s) { return decodeURIComponent(s.replace(pl, " ")); };
   var query  = window.location.search.substring(1);
+  var match;
 
   while (match = search.exec(query)){
-    URL_PARAMS[decode(match[1])] = decode(match[2]);
+    object[decode(match[1])] = decode(match[2]);
   }
 }
